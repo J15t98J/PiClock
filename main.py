aacbaebb6d25f842d21cc662a441b5d5a4af98a4
J15t98J <admin@J15t98J.co.uk>
2017-07-24 18:16:23 +0100

    Clock label scaling (long tap then swipe up/down)

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.properties import StringProperty
 from kivy.uix.button import Button
 from kivy.uix.effectwidget import HorizontalBlurEffect, VerticalBlurEffect
 from kivy.uix.image import AsyncImage
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 from kivy.uix.widget import Widget
 
@@ -22,6 +23,9 @@ class ClockProgram(Widget):
 class AsyncTouchImage(AsyncImage):
     src = "https://source.unsplash.com/featured/?nature" # TODO: allow setting custom search term via settings; TODO: night image mode?
     startTime = 0
+    touchEvents = {}
+    hasScaleEvent = False
+    isScaling = False
 
     def __init__(self, **kwargs):
         super(AsyncTouchImage, self).__init__(**kwargs)
@@ -38,22 +42,45 @@ class AsyncTouchImage(AsyncImage):
         image.bind(on_load=self.setImage)
         self.src = self.src + "#" # refuses to change picture if src URL doesn't change
 
+    def activateLabelZoom(self, dt):
+        self.isScaling = True
+        self.scaleFrom = App.get_running_app().instance.clocklabel.font_size
+
     def on_touch_down(self, touch):
         app = App.get_running_app().instance
-        # ensure touch is inside bg pic but outside clock label
-        if self.collide_point(*touch.pos) and not app.clocklabel.collide_point(*touch.pos) and app.screen_manager.current == "main":
+        # ensure touch is inside bg image and on main screen
+        if self.collide_point(*touch.pos) and app.screen_manager.current == "main":
+            if app.clocklabel.collide_point(*touch.pos):
+                # user is trying to scale the clock label
+                if not self.hasScaleEvent:
+                    self.touchEvents[touch] = [Clock.schedule_once(self.activateLabelZoom, 1), "label", touch.pos]
+                    self.hasScaleEvent = True
+            else:
+                # user is trying to change the bg image
+                self.touchEvents[touch] = [Clock.schedule_once(self.cycleImage, 1), "bg"]
             self.startTime = time.time()
-            self.event = Clock.schedule_once(self.cycleImage, 1)
+
         return True
 
     def on_touch_up(self, touch):
         timeDiff = time.time() - self.startTime
         # Picture change requires long press
+        if touch in self.touchEvents and self.touchEvents[touch][1] == "label":
+            self.hasScaleEvent = False
+            self.isScaling = False
         if timeDiff < 1000:
-            Clock.unschedule(self.event)
-        else:
-            pass # TODO: show/hide for macro buttons
+            if touch in self.touchEvents:
+                Clock.unschedule(self.touchEvents[touch][0])
+            # TODO: show/hide for macro buttons
         return True
+
+    def on_touch_move(self, touch):
+        firstTouch = self.touchEvents[touch] if touch in self.touchEvents else None
+        if firstTouch and firstTouch[1] == "label" and self.isScaling:
+            app = App.get_running_app().instance
+            posDiff = (touch.pos[0] - firstTouch[2][0], touch.pos[1] - firstTouch[2][1])
+            distance = (posDiff[0]**2 + posDiff[1]**2)**0.5
+            app.clocklabel.font_size = self.scaleFrom + distance if posDiff[1] > 0 else self.scaleFrom - distance
 
 
 class CustomButton(Button):
@@ -69,6 +96,10 @@ class CustomButton(Button):
         elif instance == "exit":
             app.effWid.effects = []
             app.screen_manager.current = "main"
+
+
+class CustomLabel(Label):
+    pass
 
 
 class MainScreen(Screen):
